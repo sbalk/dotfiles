f6d5275bcaf5fd016979b026e6436c335806038b
Bas Nijholt <bas@nijho.lt>
2025-04-04 10:29:32 -0700

    Adding `configs/shell/main.sh`

---
+++ configs/shell/main.sh
@@ -0,0 +1,11 @@
+# main.sh - can be sourced in .bash_profile/.bashrc or .zshrc
+
+source ~/dotfiles/configs/shell/00_prefer_zsh.sh  # no-op in zsh
+[ -n "$ZSH_VERSION" ]&& source ~/dotfiles/configs/shell/05_zsh_completions.sh
+source ~/dotfiles/configs/shell/10_aliases.sh
+source ~/dotfiles/configs/shell/20_exports.sh
+source ~/dotfiles/configs/shell/30_misc.sh
+source ~/dotfiles/configs/shell/40_keychain.sh
+source ~/dotfiles/configs/shell/50_python.sh
+source ~/dotfiles/configs/shell/60_slurm.sh
+[ -n "$ZSH_VERSION" ] && source ~/dotfiles/configs/shell/70_zsh_plugins.sh
