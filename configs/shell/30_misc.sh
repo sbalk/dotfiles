e7be77b058ae9a4fb7447adfeebc179a4701b740
Bas Nijholt <bas@nijho.lt>
2025-04-05 23:15:18 -0700

    Atuin fix for ZFS

--- configs/shell/30_misc.sh
+++ configs/shell/30_misc.sh
@@ -5,6 +5,20 @@ if [ -f "/opt/homebrew/bin/brew" ]; then
    eval "$(/opt/homebrew/bin/brew shellenv)"
 fi
 
+
+# atuin daemon management based on config
+is_home_on_zfs() {
+    mount | grep "$(df ~/ | tail -1 | awk '{print $1}')" | grep -q "zfs"
+    return $?
+}
+if is_home_on_zfs; then
+  export ATUIN_CONFIG_DIR="$HOME/.config/atuin/zfs"
+  if ! pgrep -f "atuin daemon" >/dev/null; then
+    echo "Starting Atuin daemon based on config"
+    nohup atuin daemon >/dev/null 2>&1 &
+  fi
+fi
+
 # Dotbins
 [ -n "$ZSH_VERSION" ] && source "$HOME/.dotbins/shell/zsh.sh"
 [ -n "$BASH_VERSION" ] && source "$HOME/.dotbins/shell/bash.sh"
