5912ad0ad49c575da0d21f7ac11f070db5d9a21f
Bas Nijholt <bas@nijho.lt>
2025-07-02 21:18:37 -0700

    Rice my GRUB

--- configs/nixos/configuration.nix
+++ configs/nixos/configuration.nix
@@ -22,6 +22,11 @@ in
     efiSupport = true;
     useOSProber = true;
     device = "nodev";
+    memtest86.enable = true;
+    theme = pkgs.sleek-grub-theme.override {
+      withStyle = "orange";
+      withBanner = "Welcome Bas!";
+    };
   };
   boot.loader.efi.canTouchEfiVariables = true;
 
