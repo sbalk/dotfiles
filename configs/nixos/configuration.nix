622b66395479870acf3e559f66d36c7248a7b08d
Bas Nijholt <bas@nijho.lt>
2025-07-19 20:20:22 -0700

    Add audio fixes

--- configs/nixos/configuration.nix
+++ configs/nixos/configuration.nix
@@ -57,6 +57,15 @@ in
     "wifi.powersave" = 2;
   };
 
+  # --- Audio ---
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;  # This provides PulseAudio compatibility
+    jack.enable = true;   # For compatibility with JACK applications
+  };
+
   # --- Bluetooth & Xbox Controller ---
   services.blueman.enable = true;
   hardware.bluetooth = {
