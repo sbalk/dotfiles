d6dc59c8b076c6e2c85b0267fae69510280e209a
Bas Nijholt <bas@nijho.lt>
2025-07-22 11:18:18 -0700

    Lazygit config

--- configs/shell/20_exports.sh
+++ configs/shell/20_exports.sh
@@ -8,3 +8,4 @@ export UPLOAD_FILE_TO="transfer.sh"  # For upload-file.sh
 export PATH="$HOME/.local/bin:$PATH"  # Common place, e.g., my upload-file script
 export PATH="/nix/var/nix/profiles/default/bin:$PATH"  # nix path
 export OLLAMA_HOST=http://pc.local:11434
+export XDG_CONFIG_HOME="$HOME/.config"
