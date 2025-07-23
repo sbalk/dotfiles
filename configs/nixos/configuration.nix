89dbf9ad0dbd3290d9d3f321843296a31a29325a
Bas Nijholt <bas@nijho.lt>
2025-07-22 22:14:29 -0700

    Workaround Hyprland NVIDIA VRAM bug on NixOS, was using 3.2GB now 800 MB on 5K
    https://github.com/NVIDIA/egl-wayland/issues/126#issuecomment-2379945259
    https://github.com/hyprwm/Hyprland/issues/7704#issuecomment-2639212608

--- configs/nixos/configuration.nix
+++ configs/nixos/configuration.nix
@@ -5,6 +5,24 @@
 
 let
   home-manager = builtins.fetchTarball "https://github.com/nix-community/home-manager/archive/master.tar.gz";
+
+  # The following is a workaround for the NVIDIA VRAM leak on Wayland.
+  # Hyprland was using >3GB of VRAM after a day of use
+  # See: https://github.com/NVIDIA/egl-wayland/issues/126#issuecomment-2379945259
+  limitFreeBufferProfile = builtins.toJSON {
+    rules = [
+      # See https://github.com/hyprwm/Hyprland/issues/7704#issuecomment-2639212608
+      # for tip about using `.Hyprland-wrapped` instead of `hyprland`.
+      { pattern = { feature = "procname"; matches = ".Hyprland-wrapped"; };
+        profile = "Limit Free Buffer Pool On Wayland Compositors"; }
+      { pattern = { feature = "procname"; matches = "gnome-shell"; };
+        profile = "Limit Free Buffer Pool On Wayland Compositors"; }
+    ];
+    profiles = [
+      { name = "Limit Free Buffer Pool On Wayland Compositors";
+        settings = [{ key = "GLVidHeapReuseRatio"; value = 0; }]; }
+    ];
+  };
 in
 {
   imports = [
@@ -40,7 +58,7 @@ in
     powerManagement.enable = true;
     open = false;
     nvidiaSettings = true;
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
+    package = config.boot.kernelPackages.nvidiaPackages.latest;
   };
   hardware.nvidia-container-toolkit.enable = true;
   hardware.graphics.enable = true;
@@ -489,6 +507,10 @@ in
     NIXOS_OZONE_WL = "1"; # covers all nixpkgs-wrapped Chromium/Electron apps
     ELECTRON_OZONE_PLATFORM_HINT = "auto"; # covers Flatpak/AppImage/binaries that bypass the wrapper
   };
+
+  # NVIDIA VRAM leak workaround, see comment at top.
+  environment.etc."nvidia/nvidia-application-profiles-rc.d/50-limit-free-buffer-pool-in-wayland-compositors.json".text =
+    limitFreeBufferProfile;
 
   # ===================================
   # Home Manager Configuration
